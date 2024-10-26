e591f652bb357c186a25ab2cc011a26cdf1ed9f3
Vamshidhar reddy Boda <vamshidharreddyboda@Vamshidhars-MacBook-Pro.local>
2024-10-26 05:49:44 +0530

    api Project setup

---
+++ ResidenthoodAdminApi/Controllers/LeaseController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Residenthood.Admin.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LeaseController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<LeaseController> _logger;
+
+        public LeaseController(ILogger<LeaseController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetLeases")]
+        [Route("GetLeases")]
+        public IEnumerable<Lease> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new Lease
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
